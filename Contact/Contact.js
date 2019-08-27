1ec37ae36d8bdb8e61069c11956d183e65cfbd22
wmboyle2 <wmboyle2@ncsu.edu>
2019-08-27 19:03:36 -0400

    Fixed error where school address wasn't showing up.

--- Contact/Contact.js
+++ Contact/Contact.js
@@ -7,8 +7,8 @@ function nameSchoolAddress(className) {
 	var schoolAddrs = document.getElementsByClassName(className);
 	
 	var currDate = new Date();
-	var month = currDate.getMonth() // 4 = May, 7 = August
-	var day = currDate.getDate() // First day = 1
+	var month = currDate.getMonth(); // 4 = May, 7 = August
+	var day = currDate.getDate(); // First day = 1
 
 	if ((month == 4 && day >= 11) || (month == 5) || (month == 6)
 			|| (month == 7 && day <= 13)) { // between May 11 and August 13
@@ -17,8 +17,8 @@ function nameSchoolAddress(className) {
 		}
 	} else {
 		for (var i = 0; i < schoolAddrs.length; i++) {
-			schoolAddrs[i].innerHTML = "2221 Dunn Avenue"<br>"03334 Bagwell Hall"<br>"Raleigh, NC 27607"
+			schoolAddrs[i].innerHTML = "2221 Dunn Avenue<br>03334 Bagwell Hall<br>Raleigh, NC 27607";
 		}
 	}
-}
-nameSchoolAddress("schoolAddress")
+}	
+nameSchoolAddress("schoolAddress");
