f299a3862715e6a0ad49d2d3dab42c2710b25694
William Boyles <williamboyles22900@gmail.com>
2019-10-21 11:31:28 -0400

    Reformatted contect page

--- Contact/Contact.js
+++ Contact/Contact.js
@@ -5,21 +5,16 @@
 
 /** Changes Address to N/A in summer [May 11 - August 13] */
 function nameSchoolAddress(className, addressLineInnerHTML) {
-	var schoolAddrs = document.getElementsByClassName(className);
-	
+	var schoolAddrs = document.getElementById(className);
+
 	var currDate = new Date();
 	var month = currDate.getMonth(); // 4 = May, 7 = August
 	var day = currDate.getDate(); // First day = 1
 
-	if ((month == 4 && day >= 11) || (month == 5) || (month == 6)
-			|| (month == 7 && day <= 13)) { // between May 11 and August 13
-		for (var i = 0; i < schoolAddrs.length; i++) {
-			schoolAddrs[i].innerHTML = "N/A in summer";
-		}
+	if((month == 4 && day >= 11) || (month == 5) || (month == 6) || (month == 7 && day <= 13)) { // 5/11-8/13
+		schoolAddrs.innerHTML = "N/A in summer";
 	} else {
-		for (var i = 0; i < schoolAddrs.length; i++) {
-			schoolAddrs[i].innerHTML = addressLineInnerHTML;
-		}
+		schoolAddrs.innerHTML = addressLineInnerHTML;
 	}
-}	
+}
 nameSchoolAddress("schoolAddress", "2221 Dunn Avenue<br>03334 Bagwell Hall<br>Raleigh, NC 27607");
